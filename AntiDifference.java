3629384e63aed7d3893cb6b00302ca57641d17d9
YsacAdvincula <152839626+YsacAdvincula@users.noreply.github.com>
2024-05-08 12:37:24 +0000

    NumericalMethods

--- AntiDifference.java
+++ AntiDifference.java
@@ -1,19 +1,21 @@
 public class AntiDifference {
     public static void main(String[] args) {
-        for (int m = 0; m <= 20; m++) {
-            System.out.println("m = " + m + ", Anti-difference of x^-" + m + " = " + antiDifference(m));
-        }
-        System.out.println("Programmed by: Ysac Advincula");
+        printNeiTable(20);
     }
 
-    public static double antiDifference(int m) {
-        if (m == 0) {
-            // The anti-difference of x^0 is x
-            return 1.0;
-        } else {
-            // The anti-difference of x^-m is -x^(1-m)/(1-m) for m != 1
-            // and log(x) for m = 1
-            return m == 1 ? 0.0 : -1.0 / (1 - m);
+    static void printNeiTable(int n) {
+        System.out.print("\nm\t\tIntegral\n---------------------------\n");
+        for (int m = 0; m <= n; m++) {
+            System.out.print(m + "\t\t");
+            if (m == 1) {
+                System.out.print("ln|x| + C\n");
+            } else if (m == 0) {
+                System.out.print("x\n");
+            } else if (m > 2) {
+                System.out.print("((x^" + (-m + 1) + ")/-" + Math.abs(-m + 1) + ") + C\n");
+            } else if (m == 2) {
+                System.out.print("-x^" + (-m + 1) + " + C\n");
+            }
         }
     }
 }
